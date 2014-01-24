017e67d9d602d2a257f5178a8e65b9186eba2d9b
Li Zhenchao <leezhenchao@gmail.com>
2014-01-24 13:23:48 +0800

    refactor: add all supported commands, and more spec for command string

--- lib/opensrs/email.rb
+++ lib/opensrs/email.rb
@@ -83,15 +83,24 @@ module Opensrs
         @socket = @connection = nil
       end
 
-      def call(command, attributes = {})
+      def make_command_string(command, attributes)
         cmd = command.to_s
         attr = attributes.map {|k, v|
           "#{k.to_s}=\"#{v.to_s}\""
         } .join(' ')
-        cmd << " #{attr}"
-        cmd = "#{cmd}\r\n.\r\n"
-        send_command(cmd)
-        receive_response
+        cmd << " #{attr}\r\n.\r\n"
+      end
+      
+      def call(command, attributes = {})
+        if not (MAIL_COMMANDS.include?(command) or
+                DOMAIN_COMMANDS.include?(command) or
+                WORKGROUP_COMMAND.include?(command))
+          raise "Command #{command.to_s} invalid"
+        else
+          cmd = make_command_string(command, attributes)
+          send_command(cmd)
+          receive_response
+        end
       end
 
       def send_command(command)
@@ -112,7 +121,7 @@ module Opensrs
         status_code = match_data[2] if match_data and match_data.length > 2
         return {
           :status => status,
-          :status_code => status_code.to_i,
+          :status_code => (status_code.to_i if status_code),
           :response_body => response
         }
       end
--- lib/opensrs/email/domain.rb
+++ lib/opensrs/email/domain.rb
@@ -1,6 +1,30 @@
 module Opensrs
   module Email
     module Domain
+      DOMAIN_COMMANDS = [
+                         :create_domain,
+                         :create_domain_alias,
+                         :delete_domain,
+                         :delete_domain_alias,
+                         :create_domain_welcome_email,
+                         :get_company_domains,
+                         :delete_domain_welcome_email,
+                         :get_domain,
+                         :get_domain_brand,
+                         :change_domain,
+                         :set_domain_admin,
+                         :set_domain_brand,
+                         :set_domain_catch_all_mailbox,
+                         :set_domain_disabled_status,
+                         :get_num_domain_mailboxes,
+                         :get_domain_mailboxes,
+                         :get_domain_mailbox_limits,
+                         :set_domain_mailbox_limits,
+                         :set_domain_allow_list,
+                         :set_domain_block_list,
+                         :get_domain_allow_list,
+                         :get_domain_block_list
+                        ]
       def create_domain(domain)
         call(:create_domain, {:domain => domain})
       end
